e274ea5d083f84fdcabe31f39d9d419f0015073b
zhouweixie <zhouweixie@126.com>
2016-10-19 14:51:06 +0800

    rabit

--- c_r.rb
+++ c_r.rb
@@ -1,6 +1,6 @@
-leg = ARGV[0]
-head = ARGV[1]
+head = ARGV[0]
+leg = ARGV[1]
 rabit = (leg.to_i - head.to_i*2)/2
 chicken = head.to_i - rabit.to_i
-puts("rabit=#{rabit} chicken=#{chicken}")
+puts("rabit=#{rabit.to_i} chicken=#{chicken.to_i}")
 
